18ab6531b4359122fe5a2838324ff79d3ae64914
Jakub Fukala <xfukal01@stud.fit.vut.cz>
2025-02-25 13:13:08 +0100

    feat: add help function and imput reading

--- parse.py
+++ parse.py
@@ -1,7 +1,25 @@
+'''
+ @file parse.py
+ @brief This is the main file of the project
+ @author Jakub Fukala (xfukal01)
+'''
+
+import sys
+from lark import Lark
+
+def call_help():
+    print("Help is here")
+
 def main():
-    print("This is the main function")
+    source_code = sys.stdin.read()
+    print(source_code)
+
 
 if __name__ == "__main__":
-    print('This program is being run by itself')
+    if "--help" in sys.argv:
+        call_help()
+        sys.exit(0)
     main()
+
+    #Konec souboru parse.py (EOF)
     
